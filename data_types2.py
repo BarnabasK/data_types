bbba1b4a62943ff9f25e07e390e3af0e52323960
= <=>
2016-10-31 13:41:13 +0300

    Final Commit

--- data_types2.py
+++ data_types2.py
@@ -1,3 +1,9 @@
+# For strings, return its length.
+# For None return string 'no value'
+# For booleans return the boolean
+# For integers return a string showing how it compares to hundred e.g. For 67 return 'less than 100' for 4034 return 'more than 100' or equal to 100 as the case may be
+# For lists return the 3rd item, or None if it doesn't exist
+
 def data_type(data):
     if data is None:
         return 'no value'
@@ -14,4 +20,4 @@ def data_type(data):
     elif isinstance(data, list):
         return data[2] if len(data) > 2 else None
 
-data_type(None)        
+print data_type(None)        
